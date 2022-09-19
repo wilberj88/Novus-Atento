f1a3d27d1e524726bf8834ee6269a9dc82e2f5ae
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-19 12:38:54 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Atento", page_icon="🤖")
+
+st.title('Novus Atento 🤖')
+st.header("Potencia tu vida con un Asistente Virtual personalizado para ti 🎯")
+
+st.write("Bienvenidos al futuro 👋")
+
+st.markdown(
+  """
+  En esta web encontrarás:
+  - 🔎 _    Diagnóstico de tu Necesidad de Atención a Clientela
+  - 🛒 _    Configuración de Asistentes Virtuales Atento exclusivos para ti 
+  - ✍️ _     Propuesta Inteligente de Contrato
+  
+  Todo lo anterior basado en:
+  - Tecnología para la optimización: Canales de Atención de Clientes Automatizado
+  - Tecnología para Análisis de Sentimiento: Construimos Asistentes Virtuales sensibles al Cliente
+  
+  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+  """
+)
