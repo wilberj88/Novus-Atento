9f93a021d413f7e765862768f807f2ebb8d07b7c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-28 17:43:52 -0500

    Update home.py

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
-st.set_page_config(layout="wide", page_title="Novus Atento", page_icon=":robot:")
+st.set_page_config(layout="wide", page_title="Novus Atento", page_icon="🤖")
 
 st.title('Novus Atento 🤖')
 st.header("Potencia tu vida con un Asistente Virtual personalizado para ti 🎯")
