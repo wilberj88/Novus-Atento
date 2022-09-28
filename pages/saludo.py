9a21007fefab432971088f79999f62957f2121ac
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-28 17:40:33 -0500

    Update saludo.py

--- pages/saludo.py
+++ pages/saludo.py
@@ -1,3 +1,7 @@
-print('Hola, soy Atento 🤖')
+import streamlit as st
+
+st.title('Hola, soy Atento 🤖')
+st.text_input('¿Cuál es tu nombre?')
+
 #person = imput("¿Cuál es tu nombre?")
 #print("Hola,", person, "soy tu nuevo asistente virtual, Atento 🤖")
