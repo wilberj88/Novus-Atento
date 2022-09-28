ee0067f78528c22180d4ed4119c7ffbc7a789425
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-28 17:48:53 -0500

    Update saludo.py

--- pages/saludo.py
+++ pages/saludo.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title('Hola, soy Atento 🤖')
 a = st.text_input('¿Cuál es tu nombre?')
 
-if a is true:
+if a:
   st.write('Hola',a, ': soy tu nuevo asistente virtual: soy tu atento 🤖')
