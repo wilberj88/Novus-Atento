b14e6da85a34f47edfbc9ef2d58665c4889a5487
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-05 17:00:23 -0500

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,29 @@ st.set_page_config(layout="wide", page_title="Novus Atento", page_icon="🤖")
 st.title('Novus Atento 🤖')
 st.header("Asistentes Virtuales para tus necesidades 🧘")
 
-st.write("Cada frente de la vida tiene su magia, elije el mejor 🧙‍ Mago 🤖 para ti")
+st.write("Cada frente de la vida tiene su magia, regístrate y elije el mejor 🧙‍ Mago 🤖 para ti")
 
-st.selectbox('Elige el Atento que requieras', ['Atento Agenda', 'Atento Clientes', 'Atento Trabajo', 'Atento Salud'])
 
+st.write("Formulario de Registro✍")
+    
+d = st.text_input('Nombre')
+
+e = st.text_input('Apellido')
+
+a = st.text_input('1. Correo electrónico')
+
+b = st.text_input('2. Ciudad ')
+
+c = st.text_input('3. Dirección?')
+
+i = st.radio('¿Cuál Atento necesitas?:', ['Atento Agenda', 'Atento Clientes', 'Atento Salud', 'Atento Trabajo', 'Atento Todos'])
+
+if a and b and c:
+  st.write('Muchas gracias, ', d, e, 'Enviamos un correo de confirmación a  <<',a, '>>. Enviaremos la facturación hacia la ciudad de <<', b, '>> a la dirección: <<', c, '>>.')
+
+
+picture = st.camera_input("Sonríe para nuestro registro")
+
+if picture:
+    st.image(picture)
+    st.write('Bienvenido a la comunidad de Atento🤖')
