223d92d4520ee9c8dd1e6ae10bbb8930e5200912
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-05 17:19:02 -0500

    Create trabajo.py

---
+++ pages/trabajo.py
@@ -0,0 +1,4 @@
+st.selectbox('Elige el rol más demandado a futuro que desees abordar', ['Data Scientist', 'Broker', 'ML Operator'])
+st.multiselect('Selecciona los problemas del planeta que deseas enfrentar', ['Hambre', 'Pobreza', 'Educacion'])
+st.multiselect('Selecciona tus principales pasatiempos', ['Leer', 'Ejercicio', 'Cine'])
+st.text_input('Indícanos actualmente en qué te ganas la vida')
