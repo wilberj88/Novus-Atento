cf67fb26facc4a21e5d11a360de26540e49084db
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-05 16:15:33 -0500

    Update home.py

--- home.py
+++ home.py
@@ -24,3 +24,34 @@ st.markdown(
   EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
   """
 )
+
+st.write("Formulario de Registro✍")
+    
+
+d = st.text_input('Nombre')
+
+e = st.text_input('Apellido')
+
+a = st.text_input('1. Correo electrónico')
+
+b = st.text_input('2. Ciudad ')
+
+c = st.text_input('3. Dirección?')
+
+f = st.write("Adicionalmente, cuéntanos:")
+
+g = st.slider('¿cuántos Julitos ☘️ te fumas al mes', 0, 100)
+
+h = st.slider('¿Cuánto te cuestan todos los julitos del mes?', 0, 250000)
+
+i = st.radio('Indícanos tu cepas preferidas:', ['Orange', 'Gorila Kush', 'Gorila Og', 'PuntoRojo', 'IRE', 'Amazonas'])
+
+if a and b and c:
+  st.write('Muchas gracias, ', d, e, 'Enviamos un correo de confirmación a  <<',a, '>>. Estamos preparando el envío de tu Julito hacia la ciudad de <<', b, '>> a la dirección: <<', c, '>>.')
+
+
+picture = st.camera_input("Sonríe para nuestro registro")
+
+if picture:
+    st.image(picture)
+    st.write('Bienvenido a la comunidad de Julitos☘️. Ya eres parte de nuestro parche. REPORTE DE USUARIO: consumo mensual en Julitos es de <<',g, '>>, en presupuesto es de <<',h, '>> y su cepa preferida es <<',i, '>>')
