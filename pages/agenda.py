8047a736989240b79b65cf8990353e2cb542fef2
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-05 17:26:59 -0500

    Update agenda.py

--- pages/agenda.py
+++ pages/agenda.py
@@ -8,7 +8,16 @@ st.set_page_config(layout="wide", page_title="Novus Atento", page_icon="🤖")
 st.title('Novus Atento 🤖')
 st.header("Configura tu Atento 🗓️ Agenda 📆")
 
-st.button('Crear mi Atento 🤖 Agenda exclusivo para mí')
+st.markdown(
+  """
+  Novus Atento Agenda te apoyará en:
+  - 🔎 _    Controlar tus metas y horarios diarios, desde el despertar hasta el acostar
+  - 🛒 _    Alarmar cumplimiento de metas diarias, semanales y mensuales
+  - ✍️ _     Recomendaciones para sa
+  
+  CONFIGURA TU ATENTO 🤖 AHORA 🕰
+  """
+)
 
 
 i = st.radio('Indícanos tu cepas preferidas:', ['Orange', 'Gorila Kush', 'Gorila Og', 'PuntoRojo', 'IRE', 'Amazonas'])
@@ -17,3 +26,4 @@ if a and b and c:
   st.write('Muchas gracias, ', d, e, 'Enviamos un correo de confirmación a  <<',a, '>>. Estamos preparando el envío de tu Julito hacia la ciudad de <<', b, '>> a la dirección: <<', c, '>>.')
 
 
+st.button('Crear mi Atento 🤖 Agenda exclusivo para mí')
