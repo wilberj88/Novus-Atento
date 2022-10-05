829cf5978565474abfb82b8160a11d8c8f16ec9e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-05 16:55:37 -0500

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide", page_title="Novus Atento", page_icon="🤖")
 st.title('Novus Atento 🤖')
 st.header("Asistentes Virtuales para tus necesidades 🧘")
 
-st.write("Cada frente de la vida tiene su magia, elije el mejor Mago 🤖🧙‍♂️🤖 para ti")
+st.write("Cada frente de la vida tiene su magia, elije el mejor 🧙‍ Mago 🤖 para ti")
 
 st.selectbox('Elige el Atento que requieras', ['Atento Agenda', 'Atento Clientes', 'Atento Trabajo', 'Atento Salud'])
 
