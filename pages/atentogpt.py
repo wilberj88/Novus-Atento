b21172f1c853dd7c52b4c80e17774cbc2056f88d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 14:11:33 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -16,6 +16,6 @@ if prompt:
     response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-    st.markdown(response)
+        st.markdown(response)
 # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
