d0a8a55bd74d8d0676a07a6b7483aa60be34a3ec
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 14:17:38 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -3,19 +3,24 @@ import numpy as np
 
 st.title('Novus Atento 🤖 tu mayordomo digital')
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-
-with st.chat_message("user"):
-    st.write("👋Bienvenido a Novus Atento 🤖: dime ¿cómo te puedo atender hoy?")
-
-prompt = st.chat_input("Tienes 30 segundos para tu respuesta...")
-if prompt:  
-    st.write(f"User has sent the following prompt: {prompt}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["user"]):
+        st.markdown(message["👋Bienvenido a Novus Atento 🤖: dime ¿cómo te puedo atender hoy?"])
+# React to user input
+if prompt := st.chat_input("Dímelo de una"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+
     response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
-# Add assistant response to chat history
+    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
+
