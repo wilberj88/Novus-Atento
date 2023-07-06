2a6725b6561d643f87cdd3c2a0cd39c6e1ad2be7
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 14:26:53 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -22,7 +22,22 @@ if prompt := st.chat_input("Dímelo de una"):
     response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        message_placeholder = st.empty()
+    full_response = ""
+    assistant_response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    # Simulate stream of response with milliseconds delay
+    for chunk in assistant_response.split():
+        full_response += chunk + " "
+        time.sleep(0.05)
+        # Add a blinking cursor to simulate typing
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": full_response})
 
