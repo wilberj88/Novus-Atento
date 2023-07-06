300d052bae6c48f1954dd19eb7df8948fb14854c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 14:29:00 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -23,21 +23,21 @@ if prompt := st.chat_input("Dímelo de una"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
-    full_response = ""
-    assistant_response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    # Simulate stream of response with milliseconds delay
-    for chunk in assistant_response.split():
-        full_response += chunk + " "
-        time.sleep(0.05)
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "▌")
-    message_placeholder.markdown(full_response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": full_response})
-
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    
