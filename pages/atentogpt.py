d6474f32bf280a35f2971f838ca0b253384abd14
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-06 14:06:50 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -8,3 +8,5 @@ with st.chat_message("user"):
 prompt = st.chat_input("Tienes 30 segundos para tu respuesta...")
 if prompt:  
     st.write(f"User has sent the following prompt: {prompt}")
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
