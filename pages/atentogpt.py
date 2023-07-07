21a5a4c70e64038785b6277cb0ba28793fe12eed
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-07-07 11:02:48 -0500

    Update atentogpt.py

--- pages/atentogpt.py
+++ pages/atentogpt.py
@@ -6,43 +6,35 @@ import time
 
 st.title('Novus Atento 🤖 tu mayordomo digital')
 
-
-# Set OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["user"]):
-        st.markdown(message["👋Bienvenido a Novus Atento 🤖: dime ¿cómo te puedo atender hoy?"])
-# React to user input
-if prompt := st.chat_input("Dímelo de una"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        assistant_response = random.choice(
-            [
-                "Hello there! How can I assist you today?",
-                "Hi, human! Is there anything I can help you with?",
-                "Do you need help?",
-            ]
-        )
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.1)
-            # Add a blinking cursor to simulate typing
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
-    
